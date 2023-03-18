c0fddbcf67bf7ba7cef9a1d186a25ab3e59e84f3
mathieuhuet <mathieuhuet@live.ca>
2023-03-18 19:22:13 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { SiGithub } from "react-icons/si"
+import { SiLinkedin } from "react-icons/si"
+import mathieu from './Assets/MathieuProfil.jpg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [ page, setPage ] = useState('main');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {page === 'main' ? <Main /> : page === 'batteryProject' ? <BatteryProject /> : <Error404 />}
     </div>
   );
+
+  function changePage ( goToPage ) {
+    setPage(goToPage);
+  }
+
+  function Main () {
+    return (
+      <div className="Main">
+        <img src={mathieu} className="Mathieu" alt="Mathieu's face spinnin'" />
+        <br />
+        <div className="All-link">
+          <a
+            className="GitHub-link"
+            href="https://github.com/mathieuhuet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiGithub />
+          </a>
+          <a
+            className="LinkedIn-link"
+            href="https://www.linkedin.com/in/mathieu--huet/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLinkedin />
+          </a>
+        </div>
+        <div className="Button-batteryProject" onClick={() => changePage('batteryProject')}>
+          Go to Battery project
+        </div>
+      </div>
+    );
+  }
+
+  function BatteryProject () {
+    return (
+      <div>
+        Battery Project
+        <div className="Button-backToMain" onClick={() => changePage('main')}>
+          Go to Main
+        </div>
+      </div>
+    );
+  }
+
+  function Error404 () {
+    return (
+      <div className="Error404">Error404, the page you tried to reach doesn't exist. Refresh your browser to return to the main page.</div>
+    );
+  }
 }
 
 export default App;
