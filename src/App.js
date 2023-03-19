2d07b0b5a8cb4dc0146d28f07b16f8c5004e0905
mathieuhuet <mathieuhuet@live.ca>
2023-03-19 00:09:43 -0400

    First rough batch of the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import { SiGithub } from "react-icons/si"
 import { SiLinkedin } from "react-icons/si"
+import { SlActionUndo } from "react-icons/sl";
+import { BiBattery } from "react-icons/bi";
+import { HiBattery0 } from "react-icons/hi2";
+import { HiBattery100 } from "react-icons/hi2";
+import { GiBatteries } from "react-icons/gi";
+import { GiBatteryPack } from "react-icons/gi";
+import { GiBatteryPackAlt } from "react-icons/gi";
+import { GiBatteryPlus } from "react-icons/gi"
+import { IoArrowBackOutline } from "react-icons/io5";
 import mathieu from './Assets/MathieuProfil.jpg';
+import batteryScreenshot1 from './Assets/BatteryScreenshot1.png';
+import batteryScreenshot2 from './Assets/BatteryScreenshot2.png';
+import batteryScreenshot3 from './Assets/BatteryScreenshot3.png';
 import './App.css';
 import { useState } from "react";
 
@@ -41,7 +53,7 @@ function App() {
           </a>
         </div>
         <div className="Button-batteryProject" onClick={() => changePage('batteryProject')}>
-          Go to Battery project
+          <BiBattery /> <GiBatteries /> <GiBatteryPack /> <GiBatteryPackAlt /> <GiBatteryPlus /> <HiBattery0 /> <HiBattery100 /> <p>Go to Battery project</p>
         </div>
       </div>
     );
@@ -49,10 +61,32 @@ function App() {
 
   function BatteryProject () {
     return (
-      <div>
-        Battery Project
+      <div className="BatteryProject">
         <div className="Button-backToMain" onClick={() => changePage('main')}>
-          Go to Main
+          <IoArrowBackOutline />
+        </div>
+        <div className="BatterySlide1">
+          <img src={batteryScreenshot1} className="BatteryScreenshot1" alt="Screenshot of Battery Monitoring App" />
+          <p>Cette application vous permet de monitorer l’état des batteries de vos IoT en temps réel. Vous avez accès à des 
+          informations comme le voltage des batteries, la température de l’équipement, la charge et la load des batteries. 
+          Sur la carte interactive vous avez tout vos équipements à leur emplacement, l’icone représente l’état de la batterie 
+          et vous pouvez cliquer dessus pour plus d’informations. L’application vous laisse aussi monitorer les performances de 
+          vos équipements, elle collecte l’état des équipements chaque 10 minutes et vous pouvez visualiser ces informations à 
+          partir d’un graphique. Vous pouvez comparer les performances entre différents équipements pour voir si certaines 
+          batteries sont entrain de se dégrader ou ont besoin d’être remplacées.</p>
+        </div>
+        <div className="BatterySlide2">
+          <img src={batteryScreenshot2} className="BatteryScreenshot2" alt="Screenshot of Battery Monitoring App" />
+          <p>J'ai construit cette application seul, le back-end et le front-end, conçu et mis en œuvre l'API sous-jacente et la 
+          modélisation des données.</p>
+        </div>
+        <div className="BatterySlide3">
+          <img src={batteryScreenshot3} className="BatteryScreenshot3" alt="Screenshot of Battery Monitoring App" />
+          <p>Cette application est alimentée par React sur le front-end. Sur le back-end, elle utilise Koa et la base de données est PostgreSQL. 
+          Les technologies supplémentaires utilisées incluent Sequelize en tant qu'ORM pour PostgreSQL, Chart.js pour les graphiques illustrant 
+          les performances des équipements, Google Map Platform pour l'API cartographique et OpenWeather pour l'API météo. L'application utilise 
+          les données météorologiques pour vous aidez à prédire les performances de vos appareils à énergie solaires et de vos appareils à énergie 
+          éoliennes.</p>
         </div>
       </div>
     );
