d8d93935263ceeee93a4d6cfd162e304087b3f8a
Mathieu Huet <mathieuhuet@live.ca>
2023-11-12 18:57:58 -0500

    feat: improved routing

--- src/Routes/routing.js
+++ src/Routes/routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Experiences from '../Pages/Experiences/experiences';
 import Mathieu from '../Pages/Mathieu/mathieu';
 import BatteryMonitoring from '../Pages/BatteryMonitoring/batterymonitoring';
